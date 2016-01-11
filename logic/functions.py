322c5d8b65ef6cab39695eb545f660c92df90a31
LMCMLJ <mjcalvey2@gmail.com>
2016-01-11 23:40:20 +0000

    Added average and lat/long
    Added functions for average calculation, and average latitude and
    longitude calculation if we add a map.

--- logic/functions.py
+++ logic/functions.py
@@ -1,21 +1,27 @@
-import json
-import sys
-import os
+import json  # import function unused for now, until we have a sample JSON file.
 
 class Processing:
     """Contains logic for processing json from wrapper.
 
-    JSON imports INCOMPLETE
+    :meanprice: defines mean price of input prices.
+    :location: defines average lat and long for input.
+    Note: JSON imports INCOMPLETE
     """
+    processeddata = {}
 
-    def meanprice(self):
-        pass
+    def average(self, *args):
+        """Simple average calculator"""
+        sumprice = sum(args)
+        totalinstances = len(args)
+        average = sumprice / totalinstances
+        Processing.processeddata.update({'meanprice': average})
+        return average
 
-    def medianprice(self):
-        pass
+    def location(self, *args1, *args2):
+        """Creates an average location for all houses analysed, in longitude and latitude.
 
-    def iqr(self):
-        pass
-
-    def location(self):
-        pass
+        Uses output from Processing.average() to determine average latitude and longitude.
+        """
+        averagelat = self.average(args1)
+        averagelong = self.average(args2)
+        Processing.processeddata.update({'averagelat': averagelat, 'averagelong': averagelong})
